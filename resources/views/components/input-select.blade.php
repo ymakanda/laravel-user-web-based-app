df56b2c2e31a6a102c72194e78d1a45c018af06f
yandiswa <ypmakanda@gmail.com>
2024-04-17 08:28:28 +0200

    Updated select options

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,7 +1,8 @@
 @props(['options'])
 
 <select {!! $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) !!}>{{$slot}}>
-   @foreach($options as $key => $value )
+<option value="" disabled selected>Please Select</option>   
+@foreach($options as $key => $value )
         <option  value="{{ $value->name }}"> {{ $value->name }}</option>
    @endforeach 
     
